3506165a7352f928e69ca049e06e87babb651bc7
00Darxk <gsturm2003@gmail.com>
2025-12-14 03:32:29 +0100

    update lab templates

--- Esercizi/lab-template/generate-lab-data-center.sh
+++ Esercizi/lab-template/generate-lab-data-center.sh
@@ -13,7 +13,7 @@ if ! [ -z "${2++}" ]; then
 fi
 
 configure_startup() {
-    echo -e "\e[1;36mNames syntax: leaf{...}, spine{...} , tof{...}, everything else is considered a server\e[0;0m"
+    echo -e "\e[1;36mNames syntax: leaf{...}, spine{...} , tof{...}, s{...} (server), c{...} (container)\e[0;0m"
     while :
     do
         read -p "Enter hostname ('q' to quit): " HOSTNAME
@@ -21,7 +21,10 @@ configure_startup() {
             break;
         fi
         cp "$LAB_TEMPLATE/startup-templates/$( echo $HOSTNAME | sed -r "s/([a-z]*).*/\1/" )-template.startup" "$LABPATH/$HOSTNAME.startup" > /dev/null 2>&1 ||
-        cp "$LAB_TEMPLATE/startup-templates/server-template.startup" "$LABPATH/$HOSTNAME.startup"
+        cp "$LAB_TEMPLATE/startup-templates/$( echo ${HOSTNAME:0:1} )erver-template.startup" "$LABPATH/$HOSTNAME.startup" > /dev/null 2>&1 ||
+        cp "$LAB_TEMPLATE/startup-templates/$( echo ${HOSTNAME:0:1} )ontainer-template.startup" "$LABPATH/$HOSTNAME.startup" > /dev/null 2>&1 ||
+        echo -e "\e[1;31mInvalid name\e[0;0m (empty .startup)"
+        echo -e "\e[1;36mCreated $LABPATH/$HOSTNAME.startup\e[0;0m"
     done
     HOSTS="$(ls "$LABPATH" | grep "[a-z]*\.startup" | sed "s/\.startup//" )"
     echo -e "\e[1;36m"
@@ -49,25 +52,29 @@ configure_startup() {
     for i in $( seq 1 $( echo "$HOSTS" | wc -l ) )
     do
         cur=$( sed -n ${i}p <<< "$HOSTS" )
+        cur_path="$( echo "$LABPATH/$cur.startup" )"
+        
         if [[ $cur == "leaf"* ]]; then
-            read -p "[$cur]> Enter IP address: " IP
-            sed -i "s/{indirizzo-lo}/${IP}/" "$LABPATH/$cur.startup"
 
+            read -p "[$cur]> Enter IP address (x.x.x.x): " IP
+            sed -i "s/{indirizzo-lo}/${IP}/" "$cur_path"
+            
             for i in "${!VXLANS[@]}"; do
                 vni=${VXLANS[$i]}
                 vlan=${VLANS[$i]}
-                star="$( echo "$LABPATH/$cur.startup" )"
                 # un-commenta tutte le righe segnate con ##
-                sed -r -i "s/^##(.*(\{vtep-vni-i\}|\{vni-i\}|\{vlan-id-i\}).*)/\1/" "$star" 
+                sed -r -i "s/^##(.*(\{vtep-vni-i\}|\{vni-i\}|\{vlan-id-i\}).*)/\1/" "$cur_path" 
                 # duplica tutte le righe da completare, segnando la copia con ##
-                sed -r -i "s/(^[^#]*(\{vtep-vni-i\}|\{vni-i\}|\{vlan-id-i\}).*)/\1\n##\1/" "$star"
+                sed -r -i "s/(^[^#]*(\{vtep-vni-i\}|\{vni-i\}|\{vlan-id-i\}).*)/\1\n##\1/" "$cur_path"
 
                 # sostituisce nelle variabili vtep-vni-i, vlan-id-i e vni-i
-                sed -r -i "s/(^[^#\n]*)(\{vtep-vni-i\})/\1vtep${vni}/" "$star"
-                sed -r -i "s/(^[^#\n]*)(\{vni-i\})/\1${vni}/" "$star"
-                sed -r -i "s/(^[^#\n]*)(\{vlan-id-i\})/\1${vlan}/" "$star"
+                sed -r -i "s/(^[^#\n]*)(\{vtep-vni-i\})/\1vtep${vni}/" "$cur_path"
+                sed -r -i "s/(^[^#\n]*)(\{vni-i\})/\1${vni}/" "$cur_path"
+                sed -r -i "s/(^[^#\n]*)(\{vlan-id-i\})/\1${vlan}/" "$cur_path"
             done
-        else
+            # elimina tutte le righe segnate con ##
+            sed -r -i "s/^##(.*(\{vtep-vni-i\}|\{vni-i\}|\{vlan-id-i\}).*)//" "$cur_path"
+        elif [[ $cur == "s"* ]] && [[ $cur != "spine"* ]]; then
             ETHS=()
             while :
             do
@@ -80,28 +87,51 @@ configure_startup() {
 
             for i in "${!VLANS[@]}"; do
                 vlan=${VLANS[$i]}
-                star="$( echo "$LABPATH/$cur.startup" )"
                 # un-commenta tutte le righe segnate con ##
-                sed -r -i "s/^##(.*\{vlan-id-j\}.*)/\1/" "$star" 
+                sed -r -i "s/^##(.*\{vlan-id-j\}.*)/\1/" "$cur_path" 
                 # duplica tutte le righe da completare, segnando la copia con ##
-                sed -r -i "s/(^[^#].*\{vlan-id-j\}.*)/\1\n##\1/" "$star"
+                sed -r -i "s/(^[^#].*\{vlan-id-j\}.*)/\1\n##\1/" "$cur_path"
 
                 # sostituisce nelle variabili vlan-id-j
-                sed -r -i "s/(^[^#\n]*)(\{vlan-id-j\})/\1${vlan}/" "$star"
+                sed -r -i "s/(^[^#\n]*)(\{vlan-id-j\})/\1${vlan}/" "$cur_path"
             done
+            # elimina tutte le righe segnate con ##
+            sed -r -i '/\{eth-i\}/! s/^##(.*\{vlan-id-j\}.*)//' "$cur_path" 
 
             for i in "${!ETHS[@]}"; do
                 eth=${ETHS[$i]}
-                
-                star="$( echo "$LABPATH/$cur.startup" )"
+                if [[ -z "${eth}" ]]; then
+                    continue
+                fi
                 # un-commenta tutte le righe segnate con ##
-                sed -r -i '/\{vlan-id-j\}/! s/^##(.*\{eth-i\}.*)/\1/' "$star" 
+                sed -r -i '/\{vlan-id-j\}/! s/^##(.*\{eth-i\}.*)/\1/' "$cur_path" 
                 # duplica tutte le righe da completare, segnando la copia con ##
-                sed -r -i "s/(^[^#].*\{eth-i\}.*)/\1\n##\1/" "$star"
+                sed -r -i "s/(^[^#].*\{eth-i\}.*)/\1\n##\1/" "$cur_path"
 
                 # sostituisce nelle variabili eth-i
-                sed -r -i "s/(^[^#\n]*)(\{eth-i\})/\1${eth}/" "$star"
+                sed -r -i "s/(^[^#\n]*)(\{eth-i\})/\1${eth}/" "$cur_path"
             done
+            # elimina tutte le righe segnate con ##
+            sed -r -i '/\{vlan-id-j\}/! s/^##(.*\{eth-i\}.*)//' "$cur_path" 
+
+        elif [[ $cur == "c"* ]]; then
+            read -p "[$cur]> Enter IP address (x.x.x.x): " IP
+            sed -i "s/{indirizzo}/${IP}/" "$cur_path"
+            read -p "[$cur]> Enter netmask (${IP}/y): " NET
+            sed -i "s/{netmask}/${NET}/" "$cur_path"
+
+            read -p "[$cur]> Enable apache2 web server? [Y/n]: " APA
+            if [ "$APA" = "y" ] || [ "$APA" = "Y" ] || [ -z "$APA" ]; then
+                sed -r -i "s/^##(.*(apache2).*)/\1/" "$cur_path" 
+                read -p "[$cur]> Enter identifier: " WEB
+                mkdir "$LABPATH/$cur" > /dev/null 2>&1 && echo -e "\e[1;36mDirectory '$LABPATH/$cur' created\e[0;0m" || echo -e "\e[1;36mDirectory '$LABPATH/$cur' already exists\e[0;0m"
+                cp -r "$LAB_TEMPLATE/web-server-template/var" "$LABPATH/$cur"
+                sed -i "s/{HOSTNAME}/${WEB}/" "$LABPATH/$cur/var/www/html/index.html"
+                read -p "[$cur]> Test web-server? [y/N] " WEB
+                if [ "$WEB" = "y" ] || [ "$WEB" = "Y" ]; then
+                    links "$LABPATH/$cur/var/www/html/index.html" > /dev/null 2>&1 || echo -e "\e[1;31m'links' not working or 'index.html' not present\e[0;0m"
+                fi
+            fi  
         fi
     done
 }
@@ -136,6 +166,7 @@ main() {
     echo -e "###########################################"
     echo -e "Default lab configuration is 2x2 Fat-Tree"
     echo -e "Manually change configuration if needed"
+    echo -e "Manually configure lab.conf"
     echo -e "###########################################\n"
 
     echo -e "This script will search the current lab directory for .startup(s) files"
@@ -156,8 +187,8 @@ main() {
     SPINES="$( ls "$LABPATH" | grep "spine\S*\.startup" | sed "s/\.startup//" )" || SPINES=""
 
     echo -e "\e[0;0m\nToFs:\e[1;34m"
-    ls "$LABPATH" | grep "tofs\S*\.startup" | sed "s/\.startup//" || echo -e "\e[0;0mno tofs"
-    TOFS="$( ls "$LABPATH" | grep "tofs\S*\.startup" | sed "s/\.startup//" )" || TOFS=""
+    ls "$LABPATH" | grep "tof\S*\.startup" | sed "s/\.startup//" || echo -e "\e[0;0mno tofs"
+    TOFS="$( ls "$LABPATH" | grep "tof\S*\.startup" | sed "s/\.startup//" )" || TOFS=""
 
     echo -e "\e[0;0m\nAttempting to create directories...\n"
 
@@ -166,7 +197,7 @@ main() {
     do
         cur=$( eval "echo \"\${$j}\"" )
         if [ -z "${cur}" ]; then
-            break
+            continue
         fi
         for i in $( seq 1 $( echo "$cur" | wc -l ) )
         do
